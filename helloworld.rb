b10235205784bc20e783379c6cb40b8cca295b69
RamadhaniRiparro <72053578+RamadhaniRiparro@users.noreply.github.com>
2021-01-21 09:04:56 +0700

    Create helloworld.rb

---
+++ helloworld.rb
@@ -0,0 +1,19 @@
+puts "hello world"
+puts "i'am human"
+
+# tihis is a comment
+=begin
+comment too
+=end
+
+print "i learn Ruby"
+print "i should study Ruby"
+puts "helo andi"
+print "breakfast with peanut"
+
+# Ruby is Easy
+
+print "i learn computer "
+print "i should study mathematic "
+puts "helo andi "
+print "breakfast with Hamburger "
