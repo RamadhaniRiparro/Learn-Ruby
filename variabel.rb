f969ce9673580772e2956b0dffa89e2310aebf52
RamadhaniRiparro <72053578+RamadhaniRiparro@users.noreply.github.com>
2021-01-21 09:33:20 +0700

    Create variabel.rb

---
+++ variabel.rb
@@ -0,0 +1,81 @@
+# learn variable
+
+name1 = "Dhani"
+name2 = "khun"
+puts name1 
+puts name2
+puts name1 + name2
+
+
+
+# Concatenate the text variable and the given string, and print the result
+text = "Let's learn programming"
+puts text + " using Progate"
+puts text + " forever"
+
+# Print the result of length * width
+length = 8
+width = 9
+puts length * width
+
+# Assign " is an awesome programming language" to the text variable
+text = " is an awesome programming language"
+
+# Concatenate the following strings with the text variable and print the result
+puts "Ruby" + text
+puts "Python" + text
+puts "Java" + text
+
+=begin
+cara update nilai dari suatu variable
+=end
+
+panjang = 9
+lebar = 8
+tinggi = 10
+puts lebar 
+puts tinggi 
+puts panjang * lebar * tinggi
+puts "-------"
+
+# Update nilai variabel dengan mengubahnya
+lebar = 13
+tinggi = 15
+puts lebar
+puts tinggi
+puts panjang * lebar * tinggi
+
+# mengubah nilai dari suatu variabel
+=begin
+x -= 10 === x = x - 10
+x += 10 === x = x + 10
+x /= 10 === x = x / 10
+x *= 10 === x = x * 10
+x %= 10 === x = x % 10
+=end
+# contoh
+puts "--------"
+puts "sebelum diupdate "
+sisi1 = 5
+sisi2 = 2
+sisi3 = 4
+sisi4 = 8
+sisi5 = 9
+puts sisi1
+puts sisi2
+puts sisi3
+puts sisi4
+puts sisi5
+
+puts "--------"
+puts "setelah diupdate "
+sisi1 += 8
+sisi2 -= 1
+sisi3 *= 9
+sisi4 /= 4
+sisi5 %= 2
+puts sisi1
+puts sisi2
+puts sisi3
+puts sisi4
+puts sisi5
