87f2e9f5ab6a771d4ca6b5cd75cbddbd2b854753
RamadhaniRiparro <ramadhani9112@gmail.com>
2021-01-21 17:23:39 +0700

    My Ruby repo

--- hash_ruby_2.rb
+++ hash_ruby_2.rb
@@ -5,6 +5,9 @@ ujian = {:subject => "IPS", :score => 97}
 # Cetak nilai dari element dengan simbol :score
 puts ujian[:score]
 
+# Penyingkatan dengan mengubah :
+# agar mempermudah penulisan
+
 # Tulis ulang hash berikut ini dengan menggunakan penyingkatan sintaks
 exam = {subject: "Matematika", score: 80}
 
