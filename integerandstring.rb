a8fc6d91d4dad4c94f0e0688e7e4172ae1976527
RamadhaniRiparro <72053578+RamadhaniRiparro@users.noreply.github.com>
2021-01-21 09:08:02 +0700

    Create integerandstring.rb

---
+++ integerandstring.rb
@@ -0,0 +1,24 @@
+#integer di Ruby
+puts 99
+puts 457
+# perkalian menggunakan tanda *
+puts 5 * 8
+# pertambahan menggunakan +
+puts 8 + 9
+# pengurangan menggunakan -
+puts 7 - 6
+# pembagian menggunakan /
+puts 15 / 3
+# modulus menggunakan %
+puts 101 % 25
+# pangkat menggunakan **
+puts 5 ** 2
+
+# string
+puts "15 + 6"
+puts "Hello world"
+
+# string concatination
+puts "hidup " + "Indonesia"
+puts "78" + "09"
+puts "78 " + "+ " + "843"
